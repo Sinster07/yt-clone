9911f30b850b076ec76f98c64d95b185ce21848d
Sanchit Srivastava <103961250+Sinster07@users.noreply.github.com>
2024-05-24 01:46:06 +0530

    first commit

---
+++ src/utils/helper.js
@@ -0,0 +1,51 @@
+var nameList = [
+  "Time",
+  "Past",
+  "Future",
+  "Dev",
+  "Fly",
+  "Flying",
+  "Soar",
+  "Soaring",
+  "Power",
+  "Falling",
+  "Fall",
+  "Jump",
+  "Cliff",
+  "Mountain",
+  "Rend",
+  "Red",
+  "Blue",
+  "Ultimate",
+  "Death",
+  "Ready",
+  "Monkey",
+  "Elevator",
+  "Wrench",
+  "Grease",
+  "Head",
+  "Theme",
+  "Grand",
+  "Cool",
+  "Kid",
+  "Boy",
+  "Girl",
+  "Vortex",
+  "Paradox",
+];
+export const generateRandomName = () => {
+  return nameList[Math.floor(Math.random() * nameList.length)];
+};
+
+export const generateRandomMessage = (length) => {
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomText = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    randomText += charset[randomIndex];
+  }
+
+  return randomText;
+};
